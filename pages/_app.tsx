e9684683f198bcf7bcc8c8e333d73bbedaad2167
ShariarSheikh <shariar.dev@gmail.com>
2022-03-08 16:11:58 +0600

    created layout file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+    // eslint-disable-next-line no-unused-vars
+    getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout ?? ((page) => page)
+    return getLayout(<Component {...pageProps} />)
+}
